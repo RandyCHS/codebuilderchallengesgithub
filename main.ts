72461d99657bed4206cfb8a50f1e08a5723b4a10
Randy Macdonald <41399033+RandyCHS@users.noreply.github.com>
2023-01-24 15:39:08 -0800

    added Challenge5A and a few extra commands

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+player.onChat("inspectDir", function (direction) {
+    player.say(agent.inspect(AgentInspection.Block, direction))
+})
 player.onChat("Challenge3", function () {
     xPos = 192
     yPos = 4
@@ -36,14 +39,25 @@ player.onChat("Challenge3", function () {
     numFwd = 3
     agent.move(FORWARD, numFwd)
 })
-player.onChat("fwdNum", function (xPos) {
-    agent.move(FORWARD, xPos)
-})
 player.onChat("Challenge9", function () {
     xPos = 230
     yPos = 4
     zPos = -15
     agent.teleport(world(xPos, yPos, zPos), EAST)
+})
+player.onChat("Challenge5A", function () {
+    xPos = 142
+    yPos = 4
+    zPos = -23
+    agent.teleport(world(xPos, yPos, zPos), NORTH)
+    sltNum = 2
+    agent.setSlot(sltNum)
+    numFwd = 10
+    for (let index = 0; index < numFwd; index++) {
+        agent.destroy(FORWARD)
+        agent.collectAll()
+        agent.move(FORWARD, 1)
+    }
 })
 player.onChat("Challenge2", function () {
     xPos = 208
@@ -204,6 +218,9 @@ player.onChat("compass", function () {
     EAST
     )
 })
+player.onChat("agInv", function (sltNum) {
+    player.say("" + agent.getItemCount(sltNum) + ":" + agent.getItemDetail(sltNum))
+})
 player.onChat("tpWdPsW", function (xPos, yPos, zPos) {
     agent.teleport(world(xPos, yPos, zPos), WEST)
 })
@@ -213,6 +230,9 @@ player.onChat("Challenge8", function () {
     zPos = -19
     agent.teleport(world(xPos, yPos, zPos), NORTH)
 })
+player.onChat("challengeNxt", function () {
+    agent.transfer(6, 2, 3)
+})
 player.onChat("Challenge1", function () {
     xPos = 229
     yPos = 4
@@ -221,17 +241,14 @@ player.onChat("Challenge1", function () {
     numFwd = 10
     agent.move(FORWARD, numFwd)
 })
-player.onChat("Challenge5", function () {
-    xPos = 145
-    yPos = 4
-    zPos = -23
-    agent.teleport(world(xPos, yPos, zPos), NORTH)
-})
 player.onChat("Challenge6", function () {
     xPos = 162
     yPos = 4
     zPos = -24
     agent.teleport(world(xPos, yPos, zPos), NORTH)
+})
+player.onChat("fwdNum", function (numFwd) {
+    agent.move(FORWARD, numFwd)
 })
 player.onChat("Challenge7", function () {
     xPos = 174
@@ -247,6 +264,7 @@ let west: Position = null
 let east: Position = null
 let p: Position = null
 let numBk = 0
+let sltNum = 0
 let numDn = 0
 let numUp = 0
 let numFwd = 0
